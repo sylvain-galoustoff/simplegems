b4395b73f477879ab46f1d11cb2a8420a2864c10
Sylvain-MED <sylvain.galoustoff@gmail.com>
2024-08-10 15:59:40 +0200

    Button : add props className and id

--- src/lib/Button/index.tsx
+++ src/lib/Button/index.tsx
@@ -3,11 +3,13 @@ import styles from "./styles.module.css";
 export type ButtonProps = {
   type?: "submit" | "button" | "reset";
   label: string;
+  id?: string;
+  className?: string;
 };
 
-function Button({ type = "button", label }: ButtonProps) {
+function Button({ type = "button", label, id, className }: ButtonProps) {
   return (
-    <button type={type} className={`${styles.button}`}>
+    <button type={type} className={`${styles.button} ${className}`} id={id}>
       {label}
     </button>
   );
