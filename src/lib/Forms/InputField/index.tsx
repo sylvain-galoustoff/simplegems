16be6be75693513412a4411edc66808d83c9782c
Sylvain-MED <sylvain.galoustoff@gmail.com>
2024-08-11 11:55:08 +0200

    InputField : add onChange props

--- src/lib/Forms/InputField/index.tsx
+++ src/lib/Forms/InputField/index.tsx
@@ -1,24 +1,35 @@
+import { ChangeEventHandler } from "react";
 import styles from "./styles.module.css";
 
 export type InputFieldProps = {
   label: string;
   id: string;
-  type: "text" | "email" | "tel" | "search" | "password";
+  type?: "text" | "email" | "tel" | "search" | "password";
   placeholder?: string;
   variant?: "primary" | "success" | "danger" | "warning";
   iconBefore?: JSX.Element;
   iconAfter?: JSX.Element;
+  value?: string;
+  onChange?: (value: string) => void;
 };
 
 function InputField({
   label,
   id,
-  type,
+  type = "text",
   variant = "primary",
   placeholder,
   iconBefore,
   iconAfter,
+  value,
+  onChange,
 }: InputFieldProps) {
+  const handleChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    if (onChange) {
+      onChange(e.target.value);
+    }
+  };
+
   return (
     <div className={`${styles.formGroup} ${styles[variant]}`}>
       <label className={`${styles.label}`} htmlFor={id}>
@@ -33,6 +44,8 @@ function InputField({
           type={type}
           placeholder={placeholder}
           id={id}
+          value={value && value}
+          onChange={handleChange}
         />
         {iconAfter && <div className={`${styles.icon} ${styles.after}`}>{iconAfter}</div>}
       </div>
