0ef85ca7495ae827a1670b50be136d6f86cfa0eb
Sylvain-MED <sylvain.galoustoff@gmail.com>
2024-08-30 15:51:42 +0200

    DebugDate : toggle show / hide

--- src/lib/Debug/DebugDate/index.tsx
+++ src/lib/Debug/DebugDate/index.tsx
@@ -1,3 +1,4 @@
+import { IoCaretUpOutline } from "react-icons/io5";
 import InputTimestamp from "./InputTimestamp";
 import InputYearMonthDay from "./InputYearMonthDay";
 import styles from "./styles.module.css";
@@ -6,35 +7,47 @@ import { useState } from "react";
 function DebugDate() {
   const [inputFormat, setInputFormat] = useState("timestamp");
   const [dateObject, setDateObject] = useState(new Date());
+  const [show, setShow] = useState(false);
+
+  const toggleShow = () => {
+    setShow(!show);
+  };
 
   return (
-    <div className={styles.debugDate}>
-      <div className={styles.inputSelector}>
-        <p
-          className={inputFormat === "timestamp" ? styles.active : undefined}
-          onClick={() => setInputFormat("timestamp")}
-        >
-          Timestamp
-        </p>
-        <p
-          className={inputFormat === "yearmonthday" ? styles.active : undefined}
-          onClick={() => setInputFormat("yearmonthday")}
-        >
-          Year, day, month
-        </p>
+    <div className={`${styles.debugDate} ${show ? styles.show : styles.hide}`}>
+      <div className={styles.actions}>
+        <div className={styles.actionsLeft}>
+          <p
+            className={inputFormat === "timestamp" ? styles.active : undefined}
+            onClick={() => setInputFormat("timestamp")}
+          >
+            Timestamp
+          </p>
+          <p
+            className={inputFormat === "yearmonthday" ? styles.active : undefined}
+            onClick={() => setInputFormat("yearmonthday")}
+          >
+            Year, day, month
+          </p>
+        </div>
+        <div className={styles.actionsRight}>
+          <IoCaretUpOutline className={styles.showButton} onClick={toggleShow} />
+        </div>
       </div>
 
-      {inputFormat === "timestamp" && (
-        <InputTimestamp inputChange={(dateObj) => setDateObject(dateObj)} />
-      )}
+      <div className={styles.debugContent}>
+        {inputFormat === "timestamp" && (
+          <InputTimestamp inputChange={(dateObj) => setDateObject(dateObj)} />
+        )}
 
-      {inputFormat === "yearmonthday" && (
-        <InputYearMonthDay inputChange={(dateObj) => setDateObject(dateObj)} />
-      )}
+        {inputFormat === "yearmonthday" && (
+          <InputYearMonthDay inputChange={(dateObj) => setDateObject(dateObj)} />
+        )}
 
-      <div className={styles.debugPart}>
-        <p className={styles.partTitle}>Date object</p>
-        <p className={styles.result}>{dateObject.toString()}</p>
+        <div className={styles.debugPart}>
+          <p className={styles.partTitle}>Date object</p>
+          <p className={styles.result}>{dateObject.toString()}</p>
+        </div>
       </div>
     </div>
   );
